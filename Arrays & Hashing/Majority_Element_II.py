c0d58ec4ed9dddde507c9cf37cc839daa7b4b068
TheGouda <44629214+TheGouda@users.noreply.github.com>
2024-08-23 16:10:53 +0530

    Majority Element II

---
+++ Arrays & Hashing/Majority_Element_II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        count_of_nums = Counter(nums)
+        # return count_of_nums
+        
+        check = n//3
+        res = []
+        
+        for i in count_of_nums:
+            if(count_of_nums[i] > check):
+                res.append(i)
+        
+        return res
+
+object = Solution()
+print(object.majorityElement([3,2,3]))  # [3]
